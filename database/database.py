8537bb2739a8fa1e2deec9e0d4e104e13b0c649b
Harsh Kushwah <sunilkush99811@gmail.com>
2025-12-25 20:15:02 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -7,5 +7,6 @@ def get_db_connection():
     Returns a SQLite3 database connection.
     """
     conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row  # Result ko dictionary jaisa access karne ke liye
+    conn.row_factory = sqlite3.Row  
     return conn
+
